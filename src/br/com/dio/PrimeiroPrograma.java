9648de828441d563ab932a02336720ff3de2d7ff
edicleiaheerdt <edicleia@heerdt.com.br>
2021-07-21 16:30:14 -0300

    test commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,10 +6,10 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		
-		Gato gato = new Gato();
+		Gato gato1 = new Gato();
 		Livros livros = new Livros();
 		
-		System.out.println(gato);
+		System.out.println(gato1);
 		System.out.println(livros);
 		
 		/*int a = 2;
